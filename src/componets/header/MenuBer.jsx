27a2678a3dea017dfdb92d9abc0931a1a777f5d8
Taposh paul <taposhp05@gmail.com>
2025-02-17 15:03:57 +0600

    create navber

--- src/componets/header/MenuBer.jsx
+++ src/componets/header/MenuBer.jsx
@@ -1,10 +1,15 @@
 import React from 'react'
+import { NavLink } from 'react-router'
 
 function MenuBer() {
     return (
-        <div>
-
-        </div>
+        <nav className='flex justify-start items-center gap-8 text-[20px] font-medium text-white'>
+            <NavLink to={"/"}>Home</NavLink>
+            <NavLink to={"#Explore"}>Explore</NavLink>
+            <NavLink to={"#Travel"}>Travel</NavLink>
+            <NavLink to={"#Blog"}>Blog</NavLink>
+            <NavLink to={"#Pricing"}>Pricing</NavLink>
+        </nav>
     )
 }
 
